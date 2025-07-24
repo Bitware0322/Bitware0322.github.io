14b2bb036fcae4157f362d009593fbe56b1c765e
Bitware0322 <zxw1999381@qq.com>
2025-07-24 19:15:54 +0800

    Site updated: 2025-07-24 19:15:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/22/NAS盒子-IPV6动态域名解析外网访问/","2025/06/24/在Github部署Hexo博客/","2025/07/23/外网通过ipv6地址访问内网服务器/","2025/07/20/电信天翼网关获取超级管理员密码/"];function toRandomPost(){
+var posts=["2025/07/22/NAS盒子-IPV6动态域名解析外网访问/","2025/07/20/电信天翼网关获取超级管理员密码/","2025/07/23/外网通过ipv6地址访问内网服务器/","2025/06/24/在Github部署Hexo博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
